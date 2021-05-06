a5a301ba3b2b0bf9e6c50a6ff4b6948fbb40e82b
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-06 14:16:56 -0700

    added app components and routing

--- reactive-application/apps/guitar-shop/src/app/app.module.ts
+++ reactive-application/apps/guitar-shop/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { InventoryDetailsComponent } from './inventory/inventory-details/inventory-details.component';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UsersComponent, UserDetailsComponent, UsersListComponent, HomeComponent, InventoryComponent, InventoryDetailsComponent, InventoryListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ reactive-application/apps/guitar-shop/src/app/routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'users', component: UsersComponent },
+  // { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class RoutingModule {}
